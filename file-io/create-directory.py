d695e454594c0805fd7e17f5a9b17941f6a18f20
Furkan Uyanık <furkanuyanik2@gmail.com>
2020-04-29 22:44:39 +0300

    File IO Functions

---
+++ file-io/create-directory.py
@@ -0,0 +1,10 @@
+import os
+
+def create_directory(directory_name):
+  if not os.path.exists(directory_name):
+    os.mkdir(directory_name)
+    print("Directory ", directory_name,  " created.")
+  else:    
+    print("Directory ", directory_name,  " already exists.")
+
+create_directory("furkan-uyanik") 
