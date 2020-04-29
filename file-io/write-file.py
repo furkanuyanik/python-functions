991ba53d03e6dfbc8deb8a16b5c56d3751cd777a
Furkan Uyanık <furkanuyanik2@gmail.com>
2020-04-29 23:17:58 +0300

    Write file

--- file-io/write-file.py
+++ file-io/write-file.py
@@ -2,5 +2,5 @@ def writeFile(file_name, content):
   f = open(file_name, "w+")
   f.write(content)
   f.close()
-
+ 
 writeFile("furkan-uyanik.txt", "Created text file for Furkan UYANIK repository.")
