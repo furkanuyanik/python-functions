0bdbbe4d3fbc686e78b09adb96f17652012fd7c1
Furkan Uyanık <furkanuyanik2@gmail.com>
2020-05-18 21:43:12 +0300

    Update replace-to-new-value.py

--- data-mining/replace-to-new-value.py
+++ data-mining/replace-to-new-value.py
@@ -1,2 +1,5 @@
 def replace_to_new_value(data, column, old_value, new_value)
     return data[column].replace(to_replace=old_value, value=new_value, inplace=True)
+
+def replace_to_mean(data, column, old_value):
+    return data[column].replace(to_replace=old_value, value=data[column].mean(), inplace=True)
